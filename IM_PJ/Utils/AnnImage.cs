a59e1389272e77296e9a4189e1b936cb22ee618b
phanh <phanh@HOANGANHPC>
2021-01-25 15:26:58 +0700

    Fix bug to update the avatar of the order

---
+++ IM_PJ/Utils/AnnImage.cs
@@ -0,0 +1,22 @@
+﻿#region .NET Framework
+using System;
+using System.Text.RegularExpressions;
+#endregion
+
+namespace IM_PJ.Utils
+{
+    public class AnnImage
+    {
+        public static string extractImage(string url)
+        {
+            var result = String.Empty;
+            var rgx = new Regex(@"[\w\d_\-\.]+$");
+            var match = rgx.Match(url);
+
+            if (match.Success)
+                result = match.Value;
+
+            return result;
+        }
+    }
+}
