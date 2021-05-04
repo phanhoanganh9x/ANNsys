0e9031eede1aaedf9733291be44d7bc80ba394d8
phanh <phanh@HOANGANHPC>
2021-05-04 18:05:55 +0700

    c

--- IM_PJ/App_Themes/Ann/js/sync-product-v2.js
+++ IM_PJ/App_Themes/Ann/js/sync-product-v2.js
@@ -1,7 +1,7 @@
 ﻿var API = "/api/v1/woocommerce/product/";
 var webList = ["ann.com.vn", "khohangsiann.com", "bosiquanao.net", "quanaogiaxuong.com", "bansithoitrang.net", "panpan.vn", "quanaoxuongmay.com", "annshop.vn", "thoitrangann.com", "nhapsionline.com"];
 var webCosmetics = ["khosimypham.com", "simyphamonline.com"];
-var cosmeticCategory = [44, 45, 56, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74];
+var cosmeticCategory = [44, 45, 56, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76];
 
 function getWeblist(categoryID) {
     // San pham thuoc danh muc my pham, nuoc hoa, thuc pham chuc nang thi them web my pham vao
