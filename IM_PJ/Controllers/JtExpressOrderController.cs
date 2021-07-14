eca9902f1279c9974a65de5bd9a5e8c29f001c04
Binh Tran <tranthaibinh111@yahoo.com>
2021-07-14 13:20:45 +0700

    Additional the order print data

---
+++ IM_PJ/Controllers/JtExpressOrderController.cs
@@ -0,0 +1,23 @@
+﻿using IM_PJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IM_PJ.Controllers
+{
+    public class JtExpressOrderController
+    {
+        public JtExpressOrder getOrder(int orderId)
+        {
+            using (var con = new inventorymanagementEntities())
+            {
+                var data = con.JtExpressOrders
+                    .Where(x => x.OrderId == orderId)
+                    .SingleOrDefault();
+
+                return data;
+            }
+        }
+    }
+}
